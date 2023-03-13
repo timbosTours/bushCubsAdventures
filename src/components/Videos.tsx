23fcdf7a636911d6f658cd4e3a08f0e73f4796b2
Timothy Fawcett <tim.fawcett89@gmail.com>
2023-03-13 14:22:00 +1100

    succesfully retrieving videos

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,41 +1,47 @@
-import React, { useEffect, useState } from 'react'
-
-import axios from 'axios'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 interface Video {
   id: string;
   title: string;
   thumbnail: string;
-} 
-
+}
 
 function Videos() {
-const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
-  const CHANNEL_ID = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID
-  
-  const [videos, setVideos] = useState<Video[]>([])
+  const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+  const CHANNEL_ID = process.env.NEXT_PUBLIC_YOUTUBE_CHANNEL_ID;
 
-    const fetchVideos = async () => {
-      const res = await axios.get(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=50
-      `);
-      console.log(res.data.items);
-      setVideos(res.data.items)
-      
-    }
+  const [videos, setVideos] = useState<Video[]>([]);
+
+  const fetchVideos = async () => {
+    const res = await axios.get(
+      `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=30`
+    );
+    const items = res.data.items;
+    const videoList = items.map((video: any) => ({
+      id: video.id.videoId,
+      title: video.snippet.title,
+      thumbnail: video.snippet.thumbnails.default.url,
+    }));
+    setVideos(videoList);
+  };
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
 
   return (
     <div>
-      <button onClick={fetchVideos}>get videos</button>
-      {videos.map((_video) => {
+      {videos.map((video) => {
         return (
-          <div key={_video.title}>
-            <p>{_video.title}</p>
-            </div>
-        )
-      })
-      }
+          <div key={video.id}>
+            <p>{video.title}</p>
+            <img src={video.thumbnail} alt={video.title} />
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Videos
+export default Videos;
